a73ced1028628251ec0b01a306fce1a2801363bc
guipleite <guipleite@gmail.com>
2020-02-17 08:10:54 -0300

    verifica se tem espacos entre os numeros

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ inp = sys.argv[1]
 def check(inp):
     for i in range(len(inp)):
         if inp[i] == " " and i>0:
-            #prev = inp[i-1]
             j=i
             while inp[j]==" ":
                 j+=1
             if inp[j].isdigit() and inp[i-1].isdigit():
-            #if inp[i+1].isdigit() and inp[i-1].isdigit() and i>0:
                 return "Erro, verifique a exprecao"
 
     inp = (inp).replace(" ", "")
