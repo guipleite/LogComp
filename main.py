f8a1b32c2323a8aabb8518e05405e9afdb94f1bf
guipleite <guipleite@gmail.com>
2020-02-17 09:06:13 -0300

    arruma alguns bugs

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import sys
 
 inp = sys.argv[1]
 
-
 def check(inp):
     for i in range(len(inp)):
         if inp[i] == " " and i>0:
             j=i
+
             while inp[j]==" ":
                 j+=1
+
             if inp[j].isdigit() and inp[i-1].isdigit():
                 return "Erro, verifique a exprecao"
 
@@ -18,16 +19,20 @@ def check(inp):
     for i in range(len(inp)):
         index+=1
 
-        if inp[i] == "+" or inp[i] == "-":
+        if i > 0 :
+            if inp[i] == "+" or inp[i] == "-" :
+              
+                if inp[i] == "+":
+                    return(int(inp[:i]) + int(check(inp[i:])))
+    
+                if inp[i] == "-":
+                    return(int(inp[:i]) - int(check(inp[i:])))
 
-            if inp[i] == "+":
-                return(int(inp[:i]) + int(check(inp[i+1:])))
-  
-            if inp[i] == "-":
-                return(int(inp[:i]) - int(check((inp[i+1:]))))
-
-        else: 
-            if len(inp) == index:
-                return inp
-
-print(check(inp))
+            else: 
+                if len(inp) == index:
+                        return int(inp)
+                        
+try:
+    print(check(inp))
+except:
+    print("Erro, verifique a exprecao")
