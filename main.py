7cafac33c5836fbd78d7903eddc20af98a93a38f
guipleite <guipleite@gmail.com>
2020-03-03 21:26:57 -0300

    progresso P1

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import sys
+
 class Token():
 
     def __init__(self,tokenType,tokenValue):
         self.tokenType = tokenType
         self.tokenValue = tokenValue
 
-class Tokenizer():
+class Tokenizer(code):
 
     def __init__(self,origin,position,actual):
         self.origin = origin
@@ -12,7 +14,7 @@ class Tokenizer():
         self.actual = None
         self.selectNext()
     
-    def selectNext(tokens):
+    def selectNext(self,tokens):
         
         if self.origin[self.positon] == " ":
             self.positon+=1
@@ -34,23 +36,61 @@ class Tokenizer():
             self.actual = int(num)
 
         else:
-            raise exeption "aaa"
+            raise Exception("Erro, verifique a exprecao")        
 
 class Parser():
 
     def __init__(self,tokens):
         self.tokens = tokens
 
-    tokens = Tokenizer(origin,position,actual)
-
     @staticmethod
     def parseExression(tokens):
 
-        pass
+        if Parser.Tokenizer.actual.tokenType.isDigit():
+            result = Parser.Tokenizer.actual.tokenValue
+
+            Parser.Tokenizer.selectNext()
+
+            while Parser.Tokenizer.actual.tokenType == "+" or Parser.Tokenizer.actual.tokenType == "-":
+                if Parser.Tokenizer.actual.tokenType =="+":
+                    Parser.Tokenizer.selectNext()
+
+                    if Parser.Tokenizer.actual.tokenType.isDigit():
+                        result+=Parser.Tokenizer.actual.tokenValue
+                    else: 
+                         raise Exception("Erro, verifique a exprecao")        
+
+                if Parser.Tokenizer.actual.tokenType =="-":
+                    Parser.Tokenizer.actual.Tokenizer.selectNext()
+
+                    if Parser.Tokenizer.actual.tokenType.isDigit():
+                        result-=Parser.Tokenizer.actual.tokenValue
+                    else: 
+                        raise Exception("Erro, verifique a exprecao")        
+            
+            return result
+
+        else: 
+            raise Exception("Erro, verifique a exprecao")        
 
     @staticmethod
     def run(code):
+        Parser.tokens = Tokenizer(code)
+        parsed = Parser.parseExpression()
+
+        return parsed
+
+
+def main():
+
+    inp = sys.argv[1]
+
+    try:
+        print(Parser.run(inp))
         
-        pass
+    except :
+        raise Exception("Erro, verifique a exprecao")        
 
 
+if __name__ == '__main__':
+    main()
