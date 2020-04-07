da079f171b431f12625dbd2b8bdaf84aba0eb6f2
guipleite <guipleite@gmail.com>
2020-04-07 16:00:04 -0300

    DS, EBNF e estrutura

--- main.py
+++ main.py
@@ -38,9 +38,38 @@ class Node():
         self.Evaluate()
 
     def Evaluate(self):
-        #return self.value
-        pass
+        return self.value
 
+class SymbolTable():
+    def __init__(self):
+        self.id_dict = {}
+
+    def getter(self, iden):
+        if identifier in self.id_dict:
+            return self.id_dict[iden]
+
+        else:
+            raise Exception("Erro, verifique a exprecao identificador nao declarado")
+
+    def setter(self, iden, value):
+        self.id_dict[iden] = value
+
+class Assignment(Node):
+    def __init__(self,value,children):
+        self.value = value
+        self.children = children
+
+    def Evaluate(self, SymbolTable):
+        SymbolTable.setter[self.value,self.children[0].Evaluate()]
+
+class Identifier(Node):
+    def __init__(self,value):
+        self.value = value
+
+    def Evaluate(self, SymbolTable):
+        result = SymbolTable.getter[self.value]
+        return result
+    
 class Tokenizer():
 
     def __init__(self,origin):
@@ -66,20 +95,50 @@ class Tokenizer():
             return None
             
         if self.origin[self.positon] == "+":
-            self.actual = Token('str' , '+')
+            self.actual = Token('operator' , '+')
             self.positon+=1
 
         elif self.origin[self.positon] == "-":
-            self.actual = Token('str' , '-')
+            self.actual = Token('operator' , '-')
             self.positon+=1
 
         elif self.origin[self.positon] == "*":
-            self.actual = Token('str' , '*')
+            self.actual = Token('operator' , '*')
             self.positon+=1
 
         elif self.origin[self.positon] == "/":
-            self.actual = Token('str' , '/')
+            self.actual = Token('operator' , '/')
             self.positon+=1
+
+        elif self.origin[self.positon] == "{":
+            self.actual = Token('command' , '{')
+            self.positon+=1
+
+        elif self.origin[self.positon] == "}":
+            self.actual = Token('command' , '}')
+            self.positon+=1
+
+        elif self.origin[self.positon] == ";":
+            self.actual = Token('endline' , ';')
+            self.positon+=1
+
+        elif self.origin[self.positon] == "$":
+            var = ''
+            self.positon+=1
+            while self.origin[self.positon].isalpha():######
+                var+=self.origin[self.positon]
+                
+                if self.positon==(len(self.origin)-1):
+                    break
+                else:
+                    self.positon+=1
+
+            self.actual = Token('iden', var)
+
+        elif self.origin[self.positon] == "=":
+            self.actual = Token('assignment' , '=')
+            self.positon+=1
+
 
         elif self.origin[self.positon] == ")" :
             self.actual = Token('str' , ')')
@@ -152,7 +211,6 @@ class UnOp(Node):
         else:
             return self.children[0].Evaluate()
 
-
 class IntVal(Node):
     def __init__(self, value, child):
         self.value = value
@@ -170,10 +228,72 @@ class NoOp(Node):
         #return ""
         pass
 
+class Commands(Node):
+    def __init__(self,children):
+        self.children = children
+    
+    def Evaluate(self):
+        for child in self.children:
+            child.Evaluate(SymbolTable)
+
 class Parser():
 
     def __init__(self,tokens):
         self.tokens = tokens
+
+    @staticmethod
+    def parseBlock():
+        if Parser.tokens.actual.tokenValue== "{":
+            commands = []
+            while Parser.tokens.actual.tokenValue != "}":
+                print("1")
+                Parser.tokens.selectNext()
+                print("2",Parser.tokens.actual.tokenValue)
+
+                commands.append(Parser.parseCommand())
+                print("ac",commands)
+
+            if Parser.tokens.actual.tokenValue== "}":
+                # print("))",node.Evaluate())
+                return Commands(commands)
+
+            else :
+                raise Exception("Erro, verifique a exprecao nao fechou }") 
+        else:
+            raise Exception("Erro, verifique a exprecao nao abriu {") 
+
+    @staticmethod
+    def parseCommand():
+
+        if True:
+
+            if Parser.tokens.actual.tokenType== "iden":
+                print("3",Parser.tokens.actual.tokenValue)
+                var = Parser.tokens.actual.tokenValue
+                
+                Parser.tokens.selectNext()
+                print("4",Parser.tokens.actual.tokenValue)
+
+                print("|",len(var),"|")
+
+                if Parser.tokens.actual.tokenValue== "=":
+                    #result = Parser.parseExpression(Parser.tokens)
+                    Parser.tokens.selectNext()
+
+                    result = Assignment(var,Parser.parseExpression(Parser.tokens))    
+                else:
+                    raise Exception("Erro, verifique a exprecao =") 
+
+            elif Parser.tokens.actual.tokenValue== "echo":
+                result = Parser.parseExpression(Parser.tokens)
+           
+            if Parser.tokens.actual.tokenValue== ";":
+
+                 #raise Exception("Erro, verifique a exprecao ;") 
+                return result
+
+        else:
+            Parser.parseBlock()
 
     @staticmethod
     def parseFactor():
@@ -203,6 +323,8 @@ class Parser():
             else:
                 return result
         else: 
+            print("5",Parser.tokens.actual.tokenValue)
+
             raise Exception("Erro, verifique a exprecao a")        
        
     @staticmethod
@@ -246,7 +368,8 @@ class Parser():
     def run(code):
         code = PrePro.filter(code)
         Parser.tokens = Tokenizer(code)
-        parsed = Parser.parseExpression(Parser.tokens)
+        Parser.table = SymbolTable()
+        parsed = Parser.parseBlock()
         return parsed.Evaluate()
 
 def main():
@@ -256,11 +379,11 @@ def main():
 
     for line in fileinput.input():
         #process(line)
-        try:
+        # try:
             print(Parser.run(line))
             
-        except :
-            raise Exception("Erro, verifique a exprecao")        
+        # except :
+        #     raise Exception("Erro, verifique a exprecao")        
 
 if __name__ == '__main__':
     main()
